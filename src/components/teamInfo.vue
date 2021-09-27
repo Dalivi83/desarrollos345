a4e497f2674604cbff275ba0f4f16a1e6a8ca8d3
crcr2020 <ccardenas@ramonbjimeno.edu.co>
2021-09-26 19:44:14 -0500

    Avance en página de equipo

---
+++ src/components/teamInfo.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-card
+              class="mx-auto"
+              max-width="400"
+            >
+              <v-img
+                class="white--text align-end"
+                height="200px"
+                :src="team.foto"
+              >
+                
+              </v-img>
+              <v-card-text class="text--primary">
+                <div>{{team.name}}</div>
+
+              </v-card-text>
+
+              <v-card-subtitle class="pb-0">{{team.rol}}
+              </v-card-subtitle>
+
+              <v-card-text class="text--primary">
+                <div>{{team.descripcion}}</div>
+
+              </v-card-text>
+
+              
+            </v-card>
+</template>
+
+<script>
+export default {
+    props: [
+        "team"
+    ],
+    
+
+}
+</script>
+
+<style>
+
+</style>
