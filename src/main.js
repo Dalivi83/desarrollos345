cf67e687ad5e1455d23ac1ca6ff38db8e737f339
jennycv <jennycarolinavr@gmail.com>
2021-10-10 22:19:12 -0500

    Creacion del crud de servicios,creacion  registro del formulario  y eventos de acciones de  servicios

--- src/main.js
+++ src/main.js
@@ -4,11 +4,19 @@ import router from './router'
 import vuetify from './plugins/vuetify'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import VueSweetalert2 from 'vue-sweetalert2';
+
+// If you don't need the styles, do not connect
+import 'sweetalert2/dist/sweetalert2.min.css';
+
+Vue.use(VueSweetalert2);
+
 
 Vue.use(VueAxios, axios)
 
 //URL base de la API
 axios.defaults.baseURL = 'http://localhost:3000/api';
+
 
 Vue.config.productionTip = false
 
