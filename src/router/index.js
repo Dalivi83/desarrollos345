b3f43b6ba6b3d6bae78fd41fb75c1378e172c0fe
gcarvajal-sjc <gloriacarvajal@msn.com>
2021-10-14 13:33:31 -0700

    Incluye seccion noticias pagina index

--- src/router/index.js
+++ src/router/index.js
@@ -36,7 +36,26 @@ const routes = [
     name: 'Form',
     component: () => import('../views/Form.vue')
   },
-
+  {
+    path: '/newsone',
+    name: 'Noticias',
+    component: () => import('../components/News/NewsOne.vue')
+  },
+  {
+    path: '/newstwo',
+    name: 'Noticias',
+    component: () => import('../components/News/NewsTwo.vue')
+  },
+  {
+    path: '/newsthree',
+    name: 'Noticias',
+    component: () => import('../components/News/NewsThree.vue')
+  },
+  {
+    path: '/newsfour',
+    name: 'Noticias',
+    component: () => import('../components/News/NewsFour.vue')
+  },
 ]
 
 const router = new VueRouter({
